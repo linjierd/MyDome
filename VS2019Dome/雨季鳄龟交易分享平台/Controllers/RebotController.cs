b89ea8f2deebcbd7d8045496dd97659a253d7019
Jey <linjie.rd@gmail.com>
2022-04-19 11:09:45 +0800

    202204191200更新

--- VS2019Dome/雨季鳄龟交易分享平台/Controllers/RebotController.cs
+++ VS2019Dome/雨季鳄龟交易分享平台/Controllers/RebotController.cs
@@ -24,6 +24,8 @@ namespace 雨季鳄龟交易分享平台.Controllers
         {
             return View();
         }
+        #region 黑名单
+        
         public IActionResult BlakList()
         {
             try
@@ -43,7 +45,7 @@ namespace 雨季鳄龟交易分享平台.Controllers
 
                 SqlConnection SqlConnection = _configuration.GetSection("SqlConnection").Get<SqlConnection>();
                 DbContext db = new DbContext(SqlConnection.BlacklistPlugin, SqlSugar.DbType.Sqlite);
-                var data = db.Db.Queryable<BlackQQ>().ToList();
+                var data = db.Db.Queryable<BlackQQ>().OrderBy(m=>m.CreateTime,SqlSugar.OrderByType.Desc).ToList();
                 ViewBag.BlackQQ = data;
                 ViewBag.IsAdmin = isAdmin;
                 #endregion
@@ -73,7 +75,127 @@ namespace 雨季鳄龟交易分享平台.Controllers
 
             SqlConnection SqlConnection = _configuration.GetSection("SqlConnection").Get<SqlConnection>();
             DbContext db = new DbContext(SqlConnection.BlacklistPlugin, SqlSugar.DbType.Sqlite);
-            return db.Db.Deleteable<BlackQQ>().In(id).ExecuteCommand();
+            var rows= db.Db.Deleteable<BlackQQ>().In(id).ExecuteCommand();
+            return rows;
+
         }
+
+        #endregion
+
+        #region 警告禁言次数
+        public IActionResult WarningTimes()
+        {
+            #region
+            //获取用户IP
+            string ip = Commond.GetHostAddress();
+            DbContext dbMaster = new DbContext();
+            var user = dbMaster.Db.Queryable<Yuji_User>().Single(m => m.yj_IP == ip);
+            bool isAdmin = user.yj_UserName == "Admin";
+            #endregion
+
+            #region 在SqlLite加载数据
+
+
+            SqlConnection SqlConnection = _configuration.GetSection("SqlConnection").Get<SqlConnection>();
+            DbContext db = new DbContext(SqlConnection.BlacklistPlugin, SqlSugar.DbType.Sqlite);
+            var data = db.Db.Queryable<WarningTime>().OrderBy(m => m.LastTime, SqlSugar.OrderByType.Desc).ToList();
+            ViewBag.WarningTimes = data;
+            ViewBag.IsAdmin = isAdmin;
+            #endregion
+            return View();
+        }
+
+        public int DelWarningQQ(int id)
+        {
+            #region 验证管理员身份
+
+            //获取用户IP
+            string ip = Commond.GetHostAddress();
+            DbContext dbMaster = new DbContext();
+            var user = dbMaster.Db.Queryable<Yuji_User>().Single(m => m.yj_IP == ip);
+
+            if (user.yj_UserName != "Admin")
+            {
+                return -1;
+            }
+            #endregion
+
+            SqlConnection SqlConnection = _configuration.GetSection("SqlConnection").Get<SqlConnection>();
+            DbContext db = new DbContext(SqlConnection.BlacklistPlugin, SqlSugar.DbType.Sqlite);
+            return db.Db.Deleteable<WarningTime>().In(id).ExecuteCommand();
+        }
+        public int ClearWarning()
+        {
+            #region 验证管理员身份
+
+            //获取用户IP
+            string ip = Commond.GetHostAddress();
+            DbContext dbMaster = new DbContext();
+            var user = dbMaster.Db.Queryable<Yuji_User>().Single(m => m.yj_IP == ip);
+
+            if (user.yj_UserName != "Admin")
+            {
+                return -1;
+            }
+            #endregion
+
+            SqlConnection SqlConnection = _configuration.GetSection("SqlConnection").Get<SqlConnection>();
+            DbContext db = new DbContext(SqlConnection.BlacklistPlugin, SqlSugar.DbType.Sqlite);
+
+            var data = db.Db.Queryable<WarningTime>().ToList();
+            foreach (var item in data)
+            {
+                item.WarningTimes = 0;
+                item.LastTime = DateTime.Now;
+            }
+            return db.Db.Updateable(data).ExecuteCommand();
+        }
+        public int ClearGag()
+        {
+            #region 验证管理员身份
+
+            //获取用户IP
+            string ip = Commond.GetHostAddress();
+            DbContext dbMaster = new DbContext();
+            var user = dbMaster.Db.Queryable<Yuji_User>().Single(m => m.yj_IP == ip);
+
+            if (user.yj_UserName != "Admin")
+            {
+                return -1;
+            }
+            #endregion
+
+            SqlConnection SqlConnection = _configuration.GetSection("SqlConnection").Get<SqlConnection>();
+            DbContext db = new DbContext(SqlConnection.BlacklistPlugin, SqlSugar.DbType.Sqlite);
+
+            var data = db.Db.Queryable<WarningTime>().ToList();
+            foreach (var item in data)
+            {
+                item.GagTimes = 0;
+                item.LastTime = DateTime.Now;
+            }
+            return db.Db.Updateable(data).ExecuteCommand();
+        }
+        public int ClearAllData()
+        {
+            #region 验证管理员身份
+
+            //获取用户IP
+            string ip = Commond.GetHostAddress();
+            DbContext dbMaster = new DbContext();
+            var user = dbMaster.Db.Queryable<Yuji_User>().Single(m => m.yj_IP == ip);
+
+            if (user.yj_UserName != "Admin")
+            {
+                return -1;
+            }
+            #endregion
+
+            SqlConnection SqlConnection = _configuration.GetSection("SqlConnection").Get<SqlConnection>();
+            DbContext db = new DbContext(SqlConnection.BlacklistPlugin, SqlSugar.DbType.Sqlite);
+            return db.Db.Deleteable<WarningTime>().ExecuteCommand();
+        }
+
+        #endregion
     }
 }
---
+++ VS2019Dome/雨季鳄龟交易分享平台/Models/SqlLite/WarningTime.cs
@@ -0,0 +1,20 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace 雨季鳄龟交易分享平台.Models.SqlLite
+{
+    [SugarTable("WarningTimes")]
+    public class WarningTime
+    {
+        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]
+        public int Id { get; set; }
+        public string GroupId { get; set; }
+        public string QQ { get; set; }
+        public int WarningTimes { get; set; }
+        public int GagTimes { get; set; }
+        public DateTime LastTime { get; set; }
+    }
+}
